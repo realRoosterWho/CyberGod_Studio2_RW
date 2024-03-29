2f2f4ad3d60ace34b0b389d276d7ad6b77ed44ed
umegoooo <152960027+umegoooo@users.noreply.github.com>
2024-03-29 21:12:16 +0800

    unity打印部位编号
    unity里能够打印每一帧鼠标放置位置信息的编号

---
+++ CyberGod_Studio2/Assets/Scripts/U2PThread.cs
@@ -0,0 +1,43 @@
+
+using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class U2PThread : MonoBehaviour
+{
+    Thread receiveThread;
+    UdpClient client;
+    public int port = 5005;
+    public bool startRecieving = true;
+    public bool printToConsole = false;
+    public string data;
+
+    void Start()
+    {
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+    }
+    private void ReceiveData()
+    {
+        client = new UdpClient(port);
+        while (startRecieving)
+        {
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] dataByte = client.Receive(ref anyIP);
+                data = Encoding.UTF8.GetString(dataByte);
+
+                 print(data); 
+            }
+            catch (Exception err)
+            {
+                print(err.ToString()); 
+            }
+        }
+    }
+}
