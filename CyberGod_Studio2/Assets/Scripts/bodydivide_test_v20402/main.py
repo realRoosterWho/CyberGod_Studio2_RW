a2c11a244fb38b6b7df3f8f52c0cfb566be3ea4a
umegoooo <152960027+umegoooo@users.noreply.github.com>
2024-04-11 21:38:48 +0800

    修正了距离计算函数/if_correct_capture能够正常判别

--- CyberGod_Studio2/Assets/Scripts/bodydivide_test_v20402/main.py
+++ CyberGod_Studio2/Assets/Scripts/bodydivide_test_v20402/main.py
@@ -84,7 +84,7 @@ class Position():  # 向量与部位，存储了两个Point（这是可以的嘛
         self.e2 = e2
 
     def length_of_vector(self):
-        return math.sqrt(math.pow(self.e1, 2) + math.pow(self.e2, 2))
+        return math.sqrt(math.pow((self.e1.x - self.e2.x), 2) + math.pow((self.e1.y - self.e2.y), 2))
 
     def k_of_vextor(self):
         return self.e1 / self.e2
@@ -172,105 +172,111 @@ if __name__ == "__main__":
             imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
             if bboxInfo:
-                if_correct_capture = 1
+                # 1肩膀距离判断
                 p11 = DirectPoint(lmList, 11, img.shape[0])
                 p12 = DirectPoint(lmList, 12, img.shape[0])
-                p14 = DirectPoint(lmList, 14, img.shape[0])
-                p16 = DirectPoint(lmList, 16, img.shape[0])
-                p23 = DirectPoint(lmList, 23, img.shape[0])
-                p24 = DirectPoint(lmList, 24, img.shape[0])
-                p25 = DirectPoint(lmList, 25, img.shape[0])
-                p26 = DirectPoint(lmList, 26, img.shape[0])
-                # 初始化了五个部位对应的lm
-                # [0]左上臂11,13;[1]左下臂13,15;[2]左大腿23,25;[3]右大腿24,26;[4]心脏*
-                positionList = [Position(p12, p14), Position(p14, p16),
-                                Position(p23, p25), Position(p24, p26)]
+                # 2脸部lm倒置判断
+                p0 = DirectPoint(lmList, 0, img.shape[0])
+                p9 = DirectPoint(lmList, 9, img.shape[0])
+                p10 = DirectPoint(lmList, 10, img.shape[0])
+                if Position(p11, p12).length_of_vector() > 100 and min(p10.y, p9.y) > p0.y:
+                    if_correct_capture = 1
+                    p14 = DirectPoint(lmList, 14, img.shape[0])
+                    p16 = DirectPoint(lmList, 16, img.shape[0])
+                    p23 = DirectPoint(lmList, 23, img.shape[0])
+                    p24 = DirectPoint(lmList, 24, img.shape[0])
+                    p25 = DirectPoint(lmList, 25, img.shape[0])
+                    p26 = DirectPoint(lmList, 26, img.shape[0])
+                    # 初始化了五个部位对应的lm
+                    # [0]左上臂11,13;[1]左下臂13,15;[2]左大腿23,25;[3]右大腿24,26;[4]心脏*
+                    positionList = [Position(p12, p14), Position(p14, p16),
+                                    Position(p23, p25), Position(p24, p26)]
 
-                # 初始化手部四个lm
-                p15 = DirectPoint(lmList, 15, img.shape[0])
-                p17 = DirectPoint(lmList, 17, img.shape[0])
-                p19 = DirectPoint(lmList, 19, img.shape[0])
-                p21 = DirectPoint(lmList, 21, img.shape[0])
-                p34, p36 = three_equal_point(p12, p24)
-                p33, p35 = three_equal_point(p11, p23)
-                p37 = half_point(p11, p12)
-                p38 = half_point(p34, p33)
-                p39 = half_point(p36, p35)
-                p40 = half_point(p23, p24)
-                #  initialize a poly item
-                handbox = get_hand_bbox2()
-                handBboxPoly2 = Polygon(handbox)
-                # body polygons
-                testPoly20 = Polygon([p37.get_xy(), p11.get_xy(), p33.get_xy(), p38.get_xy()])
-                testPoly21 = Polygon([p37.get_xy(), p12.get_xy(), p34.get_xy(), p38.get_xy()])
-                testPoly22 = Polygon([p38.get_xy(), p33.get_xy(), p35.get_xy(), p39.get_xy()])
-                testPoly23 = Polygon([p38.get_xy(), p34.get_xy(), p36.get_xy(), p39.get_xy()])
-                testPoly24 = Polygon([p39.get_xy(), p35.get_xy(), p23.get_xy(), p40.get_xy()])
-                testPoly25 = Polygon([p39.get_xy(), p40.get_xy(), p24.get_xy(), p36.get_xy()])
-                bodyPoly = Polygon([p12.get_xy(), p11.get_xy(), p23.get_xy(), p24.get_xy()])
-                # arms polygons
-                s0_vertex = positionList[0].get_arm_box()
-                s1_vertex = positionList[1].get_arm_box()
-                s2_vertex = positionList[2].get_arm_box()
-                s3_vertex = positionList[3].get_arm_box()
-                Poly0 = Polygon(s0_vertex)
-                Poly1 = Polygon(s1_vertex)
-                Poly2 = Polygon(s2_vertex)
-                Poly3 = Polygon(s3_vertex)
-                # intersection with arms
-                s0 = intersection_area(handBboxPoly2, Poly0)
-                s1 = intersection_area(handBboxPoly2, Poly1)
-                s2 = intersection_area(handBboxPoly2, Poly2)
-                s3 = intersection_area(handBboxPoly2, Poly3)
-                s20 = 0
-                s21 = 0
-                s22 = 0
-                s23 = 0
-                s24 = 0
-                s25 = 0
-                # hand inside body
-                # if bodyPoly.intersects(handBboxPoly2):
-                s20 = intersection_area(handBboxPoly2, testPoly20)
-                s21 = intersection_area(handBboxPoly2, testPoly21)
-                s22 = intersection_area(handBboxPoly2, testPoly22)
-                s23 = intersection_area(handBboxPoly2, testPoly23)
-                s24 = intersection_area(handBboxPoly2, testPoly24)
-                s25 = intersection_area(handBboxPoly2, testPoly25)
-                dict_position = [
-                    {'position': 0, 'area': s0, 'vertex': s0_vertex},
-                    {'position': 1, 'area': s1, 'vertex': s1_vertex},
-                    {'position': 2, 'area': s2, 'vertex': s2_vertex},
-                    {'position': 3, 'area': s3, 'vertex': s3_vertex},
-                    {'position': 20, 'area': s20, 'vertex': [p37.get_xy(), p11.get_xy(), p33.get_xy(), p38.get_xy()]},
-                    {'position': 21, 'area': s21, 'vertex': [p37.get_xy(), p12.get_xy(), p34.get_xy(), p38.get_xy()]},
-                    {'position': 22, 'area': s22, 'vertex': [p38.get_xy(), p33.get_xy(), p35.get_xy(), p39.get_xy()]},
-                    {'position': 23, 'area': s23, 'vertex': [p38.get_xy(), p34.get_xy(), p36.get_xy(), p39.get_xy()]},
-                    {'position': 24, 'area': s24, 'vertex': [p39.get_xy(), p35.get_xy(), p23.get_xy(), p40.get_xy()]},
-                    {'position': 25, 'area': s25, 'vertex': [p39.get_xy(), p40.get_xy(), p24.get_xy(), p36.get_xy()]}]
-                max_position = max(dict_position, key=lambda x: x['area'])
-                # 1determine the value position
-                if max_position['area'] != 0:
-                    position = max_position['position']
-                    draw_box(max_position['vertex'], (255, 0, 0))
+                    # 初始化手部四个lm
+                    p15 = DirectPoint(lmList, 15, img.shape[0])
+                    p17 = DirectPoint(lmList, 17, img.shape[0])
+                    p19 = DirectPoint(lmList, 19, img.shape[0])
+                    p21 = DirectPoint(lmList, 21, img.shape[0])
+                    p34, p36 = three_equal_point(p12, p24)
+                    p33, p35 = three_equal_point(p11, p23)
+                    p37 = half_point(p11, p12)
+                    p38 = half_point(p34, p33)
+                    p39 = half_point(p36, p35)
+                    p40 = half_point(p23, p24)
+                    #  initialize a poly item
+                    handbox = get_hand_bbox2()
+                    handBboxPoly2 = Polygon(handbox)
+                    # body polygons
+                    testPoly20 = Polygon([p37.get_xy(), p11.get_xy(), p33.get_xy(), p38.get_xy()])
+                    testPoly21 = Polygon([p37.get_xy(), p12.get_xy(), p34.get_xy(), p38.get_xy()])
+                    testPoly22 = Polygon([p38.get_xy(), p33.get_xy(), p35.get_xy(), p39.get_xy()])
+                    testPoly23 = Polygon([p38.get_xy(), p34.get_xy(), p36.get_xy(), p39.get_xy()])
+                    testPoly24 = Polygon([p39.get_xy(), p35.get_xy(), p23.get_xy(), p40.get_xy()])
+                    testPoly25 = Polygon([p39.get_xy(), p40.get_xy(), p24.get_xy(), p36.get_xy()])
+                    bodyPoly = Polygon([p12.get_xy(), p11.get_xy(), p23.get_xy(), p24.get_xy()])
+                    # arms polygons
+                    s0_vertex = positionList[0].get_arm_box()
+                    s1_vertex = positionList[1].get_arm_box()
+                    s2_vertex = positionList[2].get_arm_box()
+                    s3_vertex = positionList[3].get_arm_box()
+                    Poly0 = Polygon(s0_vertex)
+                    Poly1 = Polygon(s1_vertex)
+                    Poly2 = Polygon(s2_vertex)
+                    Poly3 = Polygon(s3_vertex)
+                    # intersection with arms
+                    s0 = intersection_area(handBboxPoly2, Poly0)
+                    s1 = intersection_area(handBboxPoly2, Poly1)
+                    s2 = intersection_area(handBboxPoly2, Poly2)
+                    s3 = intersection_area(handBboxPoly2, Poly3)
+                    s20 = 0
+                    s21 = 0
+                    s22 = 0
+                    s23 = 0
+                    s24 = 0
+                    s25 = 0
+                    # hand inside body
+                    # if bodyPoly.intersects(handBboxPoly2):
+                    s20 = intersection_area(handBboxPoly2, testPoly20)
+                    s21 = intersection_area(handBboxPoly2, testPoly21)
+                    s22 = intersection_area(handBboxPoly2, testPoly22)
+                    s23 = intersection_area(handBboxPoly2, testPoly23)
+                    s24 = intersection_area(handBboxPoly2, testPoly24)
+                    s25 = intersection_area(handBboxPoly2, testPoly25)
+                    dict_position = [
+                        {'position': 0, 'area': s0, 'vertex': s0_vertex},
+                        {'position': 1, 'area': s1, 'vertex': s1_vertex},
+                        {'position': 2, 'area': s2, 'vertex': s2_vertex},
+                        {'position': 3, 'area': s3, 'vertex': s3_vertex},
+                        {'position': 20, 'area': s20, 'vertex': [p37.get_xy(), p11.get_xy(), p33.get_xy(), p38.get_xy()]},
+                        {'position': 21, 'area': s21, 'vertex': [p37.get_xy(), p12.get_xy(), p34.get_xy(), p38.get_xy()]},
+                        {'position': 22, 'area': s22, 'vertex': [p38.get_xy(), p33.get_xy(), p35.get_xy(), p39.get_xy()]},
+                        {'position': 23, 'area': s23, 'vertex': [p38.get_xy(), p34.get_xy(), p36.get_xy(), p39.get_xy()]},
+                        {'position': 24, 'area': s24, 'vertex': [p39.get_xy(), p35.get_xy(), p23.get_xy(), p40.get_xy()]},
+                        {'position': 25, 'area': s25, 'vertex': [p39.get_xy(), p40.get_xy(), p24.get_xy(), p36.get_xy()]}]
+                    max_position = max(dict_position, key=lambda x: x['area'])
+                    # 1determine the value position
+                    if max_position['area'] != 0:
+                        position = max_position['position']
+                        draw_box(max_position['vertex'], (255, 0, 0))
 
-                # 2determine the value if_correct_capture
+                    # 2determine the value if_correct_capture
 
-                # 3whether right hand in img and determine the value x&y(relative position)
-                hand_in = p17.whether_in_image()
-                if hand_in == 1:
-                    hand_x = handbox[0][0]/img.shape[1]
-                    # inverse y
-                    hand_y = (img.shape[0] - handbox[0][1])/img.shape[0]
+                    # 3whether right hand in img and determine the value x&y(relative position)
+                    hand_in = p17.whether_in_image()
+                    if hand_in == 1:
+                        hand_x = handbox[0][0]/img.shape[1]
+                        # inverse y
+                        hand_y = (img.shape[0] - handbox[0][1])/img.shape[0]
+                    else:
+                        hand_x = -1.
+                        hand_y = -1.
+
+                    # 4whether knees out of img
+                    knee_in = p26.whether_in_image() * p25.whether_in_image()
                 else:
-                    hand_x = -1.
-                    hand_y = -1.
-
-                # 4whether knees out of img
-                knee_in = p26.whether_in_image() * p25.whether_in_image()
+                    print('wrong capture')
 
                 data = np.array([position, if_correct_capture, knee_in, hand_in, hand_x, hand_y])
-                # data = np.array([position])
-
                 sock.sendto(str.encode(str(data)), serverAddressPort) #send info to unity
 
 
