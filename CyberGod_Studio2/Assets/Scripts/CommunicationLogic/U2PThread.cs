ab9d7160509df1b2600fc1e78e2d33205b866102
umegoooo <152960027+umegoooo@users.noreply.github.com>
2024-04-11 22:08:01 +0800

    Unity通讯接受变量顺序修改

--- CyberGod_Studio2/Assets/Scripts/CommunicationLogic/U2PThread.cs
+++ CyberGod_Studio2/Assets/Scripts/CommunicationLogic/U2PThread.cs
@@ -43,8 +43,8 @@ public class U2PThread : MonoBehaviour
 
                 string[] newdata = Regex.Split(data, "\\s+", RegexOptions.IgnoreCase);
 
-                float dataFloat = float.Parse(newdata[0]);   //右手所处部位编号，默认99
-                float ifCorrectCapture = float.Parse(newdata[1]);   //（未设定）动捕是否出现问题，默认1
+                float bbox_on = float.Parse(newdata[0]);   //是否正常动捕，为0时后续变量无效
+                float dataFloat = float.Parse(newdata[1]);   //右手所处部位编号，默认99
                 float kneeIn = float.Parse(newdata[2]);     //膝盖处标记点是否在画面内：是-1，否-0
                 float handIn = float.Parse(newdata[3]);     //右手是否在画面内：是-1，否-0
                 float handX = float.Parse(newdata[4]);      //右手相对横坐标，需要乘以光标移动范围宽度食用
