e1a0504b09c137e3303fe445264e3a813bd9d85e
realRoosterWho <gonghq@shanghaitech.edu.cn>
2024-05-03 23:28:56 +0800

    - 全局倒计时系统 	- 全局倒计时系统修了个大概，就主要等着后面最后加入资源结算的时候再看这个倒计时怎么处理吧。

---
+++ CyberGod_Studio2/Assets/Scripts/GeneralCountdown_Logic.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneralCountdown_Logic : MonoBehaviour
+{
+    [SerializeField]
+    private Scrollbar countdownScrollbar;
+
+    [SerializeField]
+    private float maxCountdownTime;
+
+    private void Start()
+    {
+        // Set the max value and current value of the slider to the max countdown time
+        countdownScrollbar.size = 1;
+        countdownScrollbar.value = 1;
+    }
+
+    private void Update()
+    {
+        // Decrease the current value of the slider every frame to simulate the countdown
+        countdownScrollbar.size -= Time.deltaTime / maxCountdownTime;
+    }
+}
