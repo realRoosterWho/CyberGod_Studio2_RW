2a518d9ba440e117094917f9f30e7cbb15ca9d50
umegoooo <152960027+umegoooo@users.noreply.github.com>
2024-05-17 19:07:28 +0800

    补全缺失mesh和materials

--- CyberGod_Studio2/Assets/Scripts/New3DError/ModelRotationByMouse.cs
+++ CyberGod_Studio2/Assets/Scripts/New3DError/ModelRotationByMouse.cs
@@ -14,11 +14,14 @@ public class ModelRotationByMouse : MonoBehaviour
             float rotation = Input.GetAxis("Mouse X") * sensitivity;
 
             // 如果m_game_manager.Instance.isPause为真，返回
+            /*
             if (m_GameManager.Instance.isPaused)
             {
                 return;
             }
+             */
             transform.Rotate(new Vector3(0, rotation, 0));
+           
         }
         
     }
